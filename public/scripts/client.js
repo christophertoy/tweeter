c427994890819f020a29529d031793c3b9db2783
Christopher <christopherstoy@gmail.com>
2020-09-10 23:04:15 +0000

    added desktop responsive stlying

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,6 @@
 
 $(document).ready(function () {
 
-  // const tweetData = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd"
-  //     },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
-
   const createTweetElement = function (tweet) {
 
     const $article = $('<article>').addClass('tweets');
@@ -50,25 +23,6 @@ $(document).ready(function () {
     $content.append($tweetText);
     $footer.append($footerData);
 
-    // can use a hybrid approach
-    //   const markup = `
-    //   <article class="tweets">
-    //   <header class ="tweet-header"> 
-    //     <img src=${tweetData.user.avatars} /> 
-    //     <p class="full-name">${tweetData.user.name}</p>
-    //     <p class="username">${tweetData.user.handle}</p>
-    //   </header>
-    //   <div class="tweet-body">
-    //     <p>${tweetData.content.text}
-    //     </p>
-    //   </div>
-    //   <footer class="tweet-footer">
-    //     <p>${tweetData.created_at}</p>
-    //   </footer>
-    // </article>
-
-
-    //   `
     return $article
   }
 
@@ -79,7 +33,6 @@ $(document).ready(function () {
     }
   }
 
-  // renderTweets(tweetData);
 
   const $postForm = $('#new-post');
 
@@ -99,6 +52,7 @@ $(document).ready(function () {
     .then((response) => {
       loadTweets();
       $('#tweet-text').val('');
+      $('.counter').text(140);
     })
   })
 
